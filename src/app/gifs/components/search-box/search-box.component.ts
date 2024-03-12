ffea4b33d4fe9c923b33a836035e72b659ee34ab
japago25andes <j.pardog@uniandes.edu.co>
2024-03-12 16:17:59 -0500

    Implementacion de @ViewChild

--- src/app/gifs/components/search-box/search-box.component.ts
+++ src/app/gifs/components/search-box/search-box.component.ts
@@ -1,4 +1,4 @@
-import { Component} from '@angular/core';
+import { Component, ElementRef, ViewChild} from '@angular/core';
 
 @Component({
   selector: 'gifs-search-box',
@@ -7,12 +7,24 @@ import { Component} from '@angular/core';
   <input type="text"
     class="form-control mb-2"
     placeholder="Buscar gifs..."
+    (keyup.enter)="searchTag()"
+    #txtTagInput
     >
   `
 })
 
 export class SearchBoxComponent{
+
+  @ViewChild('txtTagInput')
+  public tagInput!: ElementRef<HTMLInputElement>;
+
   constructor() { }
+
+  //searchTag(newTag: string){
+  searchTag(){
+    const newTag = this.tagInput.nativeElement.value;
+    console.log(newTag);
+  }
 
 }
 
