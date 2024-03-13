ca5c961ba2b02382e423fc8d30dc1e56e6c0d5e2
japago25andes <j.pardog@uniandes.edu.co>
2024-03-12 23:54:53 -0500

    Proyecto finalizado

---
+++ src/app/shared/components/lazyImage/lazyImage.component.ts
@@ -0,0 +1,27 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'shared-lazy-image',
+  templateUrl: './lazyImage.component.html',
+})
+export class LazyImageComponent implements OnInit{
+
+  @Input()
+  public url!: string;
+
+  @Input()
+  public alt!: string;
+
+  public hasLoaded: boolean = false;
+
+  ngOnInit(): void {
+    if(!this.url) throw new Error('Attribute "url" is required');
+  }
+
+  onLoaded() {
+    setTimeout(() => {
+      this.hasLoaded = true;
+    }, 1000);
+  }
+
+}
