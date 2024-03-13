0c0e39cfda838128e3be9cf440dd873292f48a1d
japago25andes <j.pardog@uniandes.edu.co>
2024-03-12 22:29:35 -0500

    Finalizacion de la implementacion inicial

--- src/app/gifs/components/card-list/card-list.component.ts
+++ src/app/gifs/components/card-list/card-list.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Gif } from '../../interfaces/gifs.interface';
 
 @Component({
   selector: 'gifs-card-list',
@@ -6,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CardListComponent{
 
-  constructor() { }
+  @Input()
+  public gifs: Gif[] = [];
+
 
 }
--- src/app/shared/components/sidebar/sidebar.component.ts
+++ src/app/shared/components/sidebar/sidebar.component.ts
@@ -1,10 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { GifsService } from '../../../gifs/services/gifs.service';
 
 @Component({
   selector: 'shared-sidebar',
   templateUrl: './sidebar.component.html',
   styleUrls: ['./sidebar.component.css']
 })
+
 export class SidebarComponent{
+
+  constructor( private gifsService: GifsService){}
+
+  get tags(): string[]{
+    return this.gifsService.tagsHistory;
+  }
+
+  searchTag(tag: string):void{
+    this.gifsService.searchTag(tag);
+  }
+
+
 
 }
